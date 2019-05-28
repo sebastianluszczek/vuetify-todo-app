0f9f2c273f377c91807181b7c973acccf2c6a469
Sebastian <luszczek.sebastian@awesome-ind.com>
2019-05-28 15:52:10 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p
+      class="red white--text"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit corporis dignissimos eaque, repudiandae distinctio ad sequi consequatur ipsum sint doloremque vitae vel, molestiae officia? Possimus ipsum consequuntur ullam a est!</p>
+    <p
+      class="pink lighten-4 red--text text--darken-4"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias sequi nostrum aspernatur fugiat repellendus ad at voluptatibus voluptate perspiciatis id in excepturi, error quos harum atque quae ratione dolor sed?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
